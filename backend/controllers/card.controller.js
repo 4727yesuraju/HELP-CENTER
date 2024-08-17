691c194633c5cefebceb7d04046edd543c53e26c
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-08-17 13:02:46 +0530

    create card controller completed

--- backend/controllers/card.controller.js
+++ backend/controllers/card.controller.js
@@ -1,3 +1,42 @@
+import Card from "../models/card.model.js";
+import { errorHandler } from "../utils/error.js";
+
+
+
+export async function createCard(req,res,next){
+    try {
+        const {title, description} = req.body;
+
+        //checking length greated then 4 
+        if(title.length < 4 || description.length < 4) {
+            return next(errorHandler(401,"title and description length must be greater then 4"));
+        }
+
+        //checking strings or not 
+        if(!isNaN(title) || !isNaN(description)){
+            return next(errorHandler(401,"title and description must be string format"));
+        }
+        const newCard = new Card({title,description});
+
+        if(newCard){
+            await newCard.save();
+
+            res.status(201).json({
+                success : true,
+                data : {
+                    _id : newCard._id,
+                    title : newCard.title,
+                    description : newCard.description
+                }
+            })
+        }else {
+            next(errorHandler(401,"invalid title and description data"));
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
 
 export async function getCard(req,res,next){
     try {
@@ -10,14 +49,6 @@ export async function getCard(req,res,next){
 export async function getCards(req,res,next){
     try {
         res.json({message : "getcards"})
-    } catch (error) {
-        next(err);
-    }
-}
-
-export async function createCard(req,res,next){
-    try {
-        res.json({message : "creadcard"})
     } catch (error) {
         next(err);
     }
--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,15 @@ import cardRoute from './routers/card.route.js'
 
 const app = express();
 
+app.use(express.json()); // to parse req.body
+
 // To check server is running
 app.get("/",(req,res)=>{
     res.send("Hello from server : )");
 })
 
 
-app.use('/api/cards',cardRoute)
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,3 +26,17 @@ app.listen(PORT,()=>{
     connnectToMongoDB();
     console.log(`server is running at port ${PORT} :)`);
 })
+
+
+app.use('/api/cards',cardRoute);
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
